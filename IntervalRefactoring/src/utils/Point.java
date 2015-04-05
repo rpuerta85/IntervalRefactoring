b586a2a54708c8841c81fe1991c20ce459b4e137
raul <raul@raul-VAIO>
2015-04-05 19:05:57 +0200

    Añadimos a la clase Punto, los metodos isLessThan,isLessOrEqualsThan y isGreaterThan

--- IntervalRefactoring/src/utils/Point.java
+++ IntervalRefactoring/src/utils/Point.java
@@ -3,11 +3,18 @@ package utils;
 public class Point {
 	protected double value;
 	
-	public Point(double value,PointType type) {
+	public Point(double value) {
 		this.value = value;
 	}
 	
+	public boolean isLessThan(double value) {
+		return this.value < value;
+	}
+	public boolean isLessOrEqualsThan(double value) {
+		return this.value <= value;
+	}
 	
-	
-	
+	public boolean isGreaterThan(double value) {
+		return this.value > value;
+	}
 }
