a6d96dd209223d53c93b40de5e7651e0cf5a1d11
raul <raul@raul-VAIO>
2015-04-05 17:43:23 +0200

    Creamos clase de Utilidad Point y PointType, para interntar refactorizar más el metodo inlude(double value) ya que comparten sentecias comunes las clases hijas

---
+++ IntervalRefactoring/src/utils/Point.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class Point {
+	protected double value;
+	
+	public Point(double value,PointType type) {
+		this.value = value;
+	}
+	
+	
+	
+	
+}
---
+++ IntervalRefactoring/src/utils/PointType.java
@@ -0,0 +1,8 @@
+package utils;
+
+public enum PointType {
+	UNTIL_POINT, 
+	EXACT_POINT, 
+	FROM_POINT;
+
+}
