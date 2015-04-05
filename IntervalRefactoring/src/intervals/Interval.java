fdc39e770fe1d7cb8837bb342577e0f026c9bb9b
raul <raul@raul-VAIO>
2015-04-05 14:01:44 +0200

    hacemos el metodo includes(Interval interval) abstracto para que cada clase hija tenga su propia implementación particular del metodo sin necesidad del enumerado que distingue el tipo de intervalo

--- IntervalRefactoring/src/intervals/Interval.java
+++ IntervalRefactoring/src/intervals/Interval.java
@@ -32,8 +32,8 @@ public abstract class Interval {
 		}
 	}*/
 	public abstract boolean includes(double value);
-	
-	public boolean includes(Interval interval) {
+	public abstract boolean includes(Interval interval);
+/*	public boolean includes(Interval interval) {
 		boolean minimumIncluded = this.includes(interval.minimum);
 		boolean maximumIncluded = this.includes(interval.maximum);
 		switch (opening) {
@@ -112,7 +112,7 @@ public abstract class Interval {
 			assert false;
 			return false;
 		}
-	}
+	}*/
 
 	public boolean intersectsWith(Interval interval) {
 		if (minimum == interval.maximum) {
