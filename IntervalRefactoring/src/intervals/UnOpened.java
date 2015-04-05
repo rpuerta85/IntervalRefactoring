3d009ecdfe673fbaa9d90ea50fc1355deef95abd
raul <raul@raul-VAIO>
2015-04-05 22:46:00 +0200

    Hacemos el mismo procedimiento para la clase UnOpened

--- IntervalRefactoring/src/intervals/UnOpened.java
+++ IntervalRefactoring/src/intervals/UnOpened.java
@@ -1,11 +1,13 @@
 package intervals;
 
 public class UnOpened extends Interval{
-
+	//protected
+	
 	public UnOpened(double minimum, double maximum, Opening opening) {
 		super(minimum, maximum, opening);
 	}
 
+	
 	@Override
 	public boolean includes(double value) {
 		return minimum <= value && value <= maximum;
