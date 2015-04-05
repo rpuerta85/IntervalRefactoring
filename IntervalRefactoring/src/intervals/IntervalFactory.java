0677d321516587d4d2e553e1ffc904843ef3b7e7
raul <raul@raul-VAIO>
2015-04-05 12:46:47 +0200

    Actualizamos constructor de la factoria de Intervalos

--- IntervalRefactoring/src/intervals/IntervalFactory.java
+++ IntervalRefactoring/src/intervals/IntervalFactory.java
@@ -4,6 +4,18 @@ package intervals;
 public class IntervalFactory {
 
 	public static Interval getInterval(double minimum, double maximum, Opening opening) {
-		return new Interval(minimum, maximum, opening);
+		switch (opening) {
+		case BOTH_OPENED:
+			return new BothOpened(minimum, maximum, opening);
+		case LEFT_OPENED:
+			return new LeftOpened(minimum, maximum, opening);
+		case RIGHT_OPENED:
+			return new RightOpened(minimum, maximum, opening);
+		case UNOPENED:
+			return new UnOpened(minimum, maximum, opening);
+		default:
+			throw new IllegalArgumentException("Incorrect type interval value");
+			
+		}
 	}
 }
