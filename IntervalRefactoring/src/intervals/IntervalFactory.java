f2469a80942fd93216f8711160226bce889d51a7
raul <raul@raul-VAIO>
2015-04-05 12:56:49 +0200

    añadido constructoe en la factoria para la clase UnOpened

--- IntervalRefactoring/src/intervals/IntervalFactory.java
+++ IntervalRefactoring/src/intervals/IntervalFactory.java
@@ -18,5 +18,16 @@ public class IntervalFactory {
 			
 		}
 	}
-	
+	public static Interval getIntervalBothOpened(double minimum, double maximum, Opening opening) {
+		return new BothOpened(minimum, maximum, opening);
+	}
+	public static Interval getIntervalLeftOpened(double minimum, double maximum, Opening opening) {
+		return new LeftOpened(minimum, maximum, opening);
+	}
+	public static Interval getIntervalRightOpened(double minimum, double maximum, Opening opening) {
+		return new RightOpened(minimum, maximum, opening);
+	}
+	public static Interval getIntervalUnOpened(double minimum, double maximum, Opening opening) {
+		return new UnOpened(minimum, maximum, opening);
+	}
 }
