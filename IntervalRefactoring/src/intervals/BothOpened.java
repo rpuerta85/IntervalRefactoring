61fe948768eb8dd39ef50d8e21893c664bd238c8
raul <raul@raul-VAIO>
2015-04-06 20:12:53 +0200

    Aplicamos doble despacho para el metodo includes(LeftOpened interval)

--- IntervalRefactoring/src/intervals/BothOpened.java
+++ IntervalRefactoring/src/intervals/BothOpened.java
@@ -9,21 +9,21 @@ public class BothOpened extends Interval{
 	public BothOpened(double minimum, double maximum, Opening opening) {
 		super(new FromPoint(minimum), new UntilPoint(maximum), opening);
 	}
-	// hay que hacer doble despacho creo para los que difieren
 
+	//Doble despacho
+	public boolean includes(LeftOpened interval) {
+		return interval.amIIncludeInInterval(this);
+	}
 	
 	@Override
-	public boolean includes(Interval interval) {// hay que hacer doble despacho creo
+	public boolean includes(Interval interval) {
 			boolean minimumIncluded = this.includes(interval.minimum2.getValue());
 			boolean maximumIncluded = this.includes(interval.maximum2.getValue());
 			switch (interval.opening) {
 				case BOTH_OPENED:
-					/*return (minimumIncluded || minimum2.getValue() == interval.minimum2.getValue())
-							&& (maximumIncluded || maximum2.getValue() == interval.maximum2.getValue());*/
 					return includesBothOpened(interval);
 				case LEFT_OPENED:
-					return (minimumIncluded || minimum2.getValue() == interval.minimum2.getValue())
-							&& (maximumIncluded);
+					return includes((LeftOpened)interval);
 				case RIGHT_OPENED:
 					return (minimumIncluded)
 							&& (maximumIncluded || maximum2.getValue() == interval.maximum2.getValue());
