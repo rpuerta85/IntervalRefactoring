baeaf144dd9cade2d412eec4bf7acc4eb0e91f6c
raul <raul@raul-VAIO>
2015-04-10 00:04:47 +0200

    hablando con comopañeros me han comentado que se puede refactorizar mas jugando con un doble despacho entre Point e Interval

--- IntervalRefactoring/src/utils/OpenPoint.java
+++ IntervalRefactoring/src/utils/OpenPoint.java
@@ -1,5 +1,7 @@
  package utils;
 
+import intervals.Interval;
+
 
 public class OpenPoint extends Point {
 
@@ -17,5 +19,6 @@ public class OpenPoint extends Point {
 		return this.value > value;
 
 	}
+	
 
 }
